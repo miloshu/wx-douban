a3a1e6a469c5ed0eaee56eada4fe25de1d317f98
shusishou <454924687@qq.com>
2018-04-16 15:31:11 +0800

    init

---
+++ pages/movie-details/movie-details.js
@@ -0,0 +1,84 @@
+// pages/movie-details/movie-details.js
+var _URL = "https://api.douban.com/v2/movie/subject/"
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movie: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.showLoading({
+      title: "加载中...",
+      mask: true,
+      success: () => {
+        wx.request({
+          url: _URL + options.id,
+          data: {},
+          header: {
+            'Content-Type': 'json'
+          },
+          success: (res) => {
+            wx.hideLoading()
+            console.log(res.data)
+            this.setData({
+              movie: res.data
+            })
+          }
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
